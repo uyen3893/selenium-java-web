444f6e9491d55fb4c1a4e9d9356d487a3ee60af2
Uyen Nguyen <uyenlocal@FVFYJ2T7HV2H.local>
2022-08-07 11:26:48 +0700

    Lab 22, finish footer test flow

--- src/test/java/test_flows/global/FooterTestFlow.java
+++ src/test/java/test_flows/global/FooterTestFlow.java
@@ -1,7 +1,16 @@
 package test_flows.global;
 
 import models.components.global.footer.FooterColumnComponent;
+import models.components.global.footer.FooterComponent;
+import models.pages.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import url.Urls;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FooterTestFlow {
 
@@ -12,29 +21,82 @@ public class FooterTestFlow {
     }
 
     public void verifyFooterComponent() {
-        verifyInformationColumn();
-        verifyCustomerServiceColumn();
-        verifyAccountColumn();
-        verifyFollowUsColumn();
+        BasePage basePage = new BasePage(driver);
+        FooterComponent footerComponent = basePage.footerComponent();
+        verifyInformationColumn(footerComponent.informationColumnComponent());
+        verifyCustomerServiceColumn(footerComponent.customerServiceColumnComponent());
+        verifyAccountColumn(footerComponent.myAccountColumnComponent());
+        verifyFollowUsColumn(footerComponent.followUsColumnComponent());
     }
 
-    private void verifyFollowUsColumn() {
+    private void verifyFollowUsColumn(FooterColumnComponent footerColumnComponent) {
+        String url = Urls.demoBaseUrl;
+        List<String> expectedLinkTexts = Arrays.asList(
+                "Facebook", "Twitter", "RSS", "YouTube", "Google+"
+        );
+        List<String> expectedHrefs = Arrays.asList(
+                "http://www.facebook.com/nopCommerce", "https://twitter.com/nopCommerce",
+                url + "/news/rss/1", "http://www.youtube.com/user/nopCommerce", "https://plus.google.com/+nopcommerce"
+        );
+        testFooterColumn(footerColumnComponent, expectedLinkTexts, expectedHrefs);
     }
 
-    private void verifyAccountColumn() {
+    private void verifyAccountColumn(FooterColumnComponent footerColumnComponent) {
+        String url = Urls.demoBaseUrl;
+        List<String> expectedLinkTexts = Arrays.asList(
+                "My account", "Orders", "Addresses", "Shopping cart", "Wishlist");
+        List<String> expectedHrefs = Arrays.asList(
+                url + "/customer/info", url + "/customer/orders", url + "/customer/addresses",
+                url + "/cart", url + "/wishlist"
+        );
+        testFooterColumn(footerColumnComponent, expectedLinkTexts, expectedHrefs);
     }
 
-    private void verifyCustomerServiceColumn() {
+    private void verifyCustomerServiceColumn(FooterColumnComponent footerColumnComponent) {
+        String url = Urls.demoBaseUrl;
+        List<String> expectedLinkTexts = Arrays.asList(
+                "Search", "News", "Blog", "Recently viewed products",
+                "Compare products list", "New products");
+        List<String> expectedHrefs = Arrays.asList(
+                url + "/search", url + "/news", url + "/blog", url + "/recentlyviewedproducts",
+                url + "/compareproducts", url + "/newproducts");
+        testFooterColumn(footerColumnComponent, expectedLinkTexts, expectedHrefs);
     }
 
-    private void verifyInformationColumn() {
+    private void verifyInformationColumn(FooterColumnComponent footerColumnComponent) {
+        String url = Urls.demoBaseUrl;
+        List<String> expectedLinkTexts = Arrays.asList(
+                "Sitemap", "Shipping & Returns", "Privacy Notice", "Conditions of Use",
+                "About us", "Contact us");
+        List<String> expectedHrefs = Arrays.asList(
+                url + "/sitemap", url + "/shipping-returns", url + "/privacy-policy", url + "/conditions-of-use",
+                url + "/about-us", url + "/contactus");
+        testFooterColumn(footerColumnComponent, expectedLinkTexts, expectedHrefs);
     }
 
-    private void testFooterColumn(FooterColumnComponent footerColumnComponent) {
-        System.out.println(footerColumnComponent.headerElem().getText());
-        footerColumnComponent.linkElems().forEach(link -> {
-            System.out.println(link.getText());
-            System.out.println(link.getAttribute("href"));
-        });
+    private void testFooterColumn(
+            FooterColumnComponent footerColumnComponent, List<String> expectedLinkTexts, List<String> expectedHrefs) {
+
+        List<String> actualLinkTexts = new ArrayList<>();
+        List<String> actualHrefs = new ArrayList<>();
+
+        for (WebElement linkElem : footerColumnComponent.linkElems()) {
+            actualLinkTexts.add(linkElem.getText().trim());
+            actualHrefs.add(linkElem.getAttribute("href"));
+        }
+
+        //Verify link texts and hyperlinks are empty or not
+        if (actualHrefs.isEmpty() || actualLinkTexts.isEmpty()) {
+            Assert.fail("[ERR] Texts or hyperlinks are empty!");
+        }
+
+        //Verify link texts
+        Assert.assertEquals(actualLinkTexts.size(), expectedLinkTexts.size(),
+                "[ERR] Actual and expected texts are different!");
+
+
+        //Verify hyperlinks
+        Assert.assertEquals(actualHrefs.size(), expectedHrefs.size(),
+                "[ERR] Actual and expected hyperlinks are different!");
     }
 }
--- src/test/java/tests/global/footer/FooterTest.java
+++ src/test/java/tests/global/footer/FooterTest.java
@@ -14,43 +14,21 @@ import url.Urls;
 
 public class FooterTest {
 
-    @Test(priority = 1)
+    @Test()
     private static void testFooterRegisterPage() {
-        String actualResult = "actual";
-        String expectedResult = "expected";
-
-//        Verifier.verifyEquals(actualResult, expectedResult);
-        //Hard assertion
-        Assert.assertEquals(actualResult, expectedResult, "[ERR] Welcome message is incorrect!");
-        Assert.assertTrue(actualResult.equals(expectedResult));
-        Assert.assertFalse(actualResult.equals(expectedResult));
-        Assert.fail();
-        Assert.fail("...");
 
     }
 
-   @Test(priority = 2)
+   @Test()
     private static void testFooterLoginPage() {
-        //Soft assertion
-       SoftAssert softAssert = new SoftAssert();
-       softAssert.assertEquals(2, 3);
-       softAssert.assertEquals(1, 1);
-       softAssert.assertEquals(5, 7);
-
-       System.out.println("Successfully");
-       softAssert.assertAll();
 
     }
 
     @Test
     private static void testFooterCategoryPage() {
-        WebDriver driver = DriverFactory.getChromeDriver();
-        driver.get(Urls.baseUrl);
-        FooterTestFlow footerTestFlow = new FooterTestFlow(driver);
-        footerTestFlow.verifyFooterComponent();
     }
 
-    @Test(priority = 3)
+    @Test()
      public void testFooterHomePage() {
         WebDriver driver = DriverFactory.getChromeDriver();
 
@@ -59,18 +37,11 @@ public class FooterTest {
             HomePage homePage = new HomePage(driver);
             InformationColumnComponent informationColumnComponent = homePage.footerComponent().informationColumnComponent();
 
-            testFooterColumn(informationColumnComponent);
         } catch (Exception ignored) {}
 
         driver.quit();
 
     }
 
-    private void testFooterColumn(FooterColumnComponent footerColumnComponent) {
-        System.out.println(footerColumnComponent.headerElem().getText());
-        footerColumnComponent.linkElems().forEach(link -> {
-            System.out.println(link.getText());
-            System.out.println(link.getAttribute("href"));
-        });
-    }
+
 }
